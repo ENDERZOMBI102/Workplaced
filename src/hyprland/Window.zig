1b701bc9172e394a69aec2292f240ec896ecc441
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2024-08-24 20:28:45 +0200

    feat: Complete window attributes

--- src/hyprland/Window.zig
+++ src/hyprland/Window.zig
@@ -7,46 +7,50 @@ address: []const u8,
 mapped: bool,
 /// Unknown.
 hidden: bool,
-/// Absolute X-coordinate of the window on the monitor (in pixels).
-x: i32,
-/// Absolute Y-coordinate of the window on the monitor (in pixels).
-y: i32,
-/// Width of the window (in pixels).
-width: u32,
-/// Height of the window (in pixels).
-height: u32,
-/// Numeric ID of the workspace which the window is on.
-workspace_id: u32,
-/// Name of the workspace which the window is on.
-workspace_name: []const u8,
+/// Absolute coordinates of the window on the monitor (in pixels).
+at: struct{ i32, i32 },
+/// Size of the window (in pixels).
+size: struct{ u32, u32 },
+/// The workspace on which the window is located on.
+workspace: struct {
+	/// Numeric ID of the workspace.
+	id: u32,
+	/// Name of the workspace.
+	name: []const u8,
+},
 /// Whether or not this is a floating window.
 floating: bool,
+/// Whether or not this window is pseudo-tiled.
+pseudo: bool,
 /// Numeric ID of the monitor which the window is on.
-monitor_id: u32,
+monitor: u32,
 /// Window manager class assigned to this window.
-wm_class: []const u8,
+class: []const u8,
 /// Current title of the window.
 title: []const u8,
 /// Window manager class when the window was created.
-initial_wm_class: []const u8,
+initialClass: []const u8,
 /// Title when the window was created.
-initial_title: []const u8,
+initialTitle: []const u8,
 /// Process ID of the process the window is assigned to.
 pid: u32,
 /// Whether or not the window is using xwayland to be displayed.
 xwayland: bool,
-/// Unknown.
+/// Whether or not the window is pinned.
 pinned: bool,
 /// Whether or not the window is in fullscreen mode.
 fullscreen: bool,
 /// Unknown.
-fullscreen_mode: u32,
+fullscreenMode: u32,
 /// Unknown.
-fake_fullscreen: bool,
-
-pub fn init() Self {
-
-}
-
+fakeFullscreen: bool,
+/// Unknown.
+grouped: [][]const u8,
+/// Unknown.
+tags: [][]const u8,
+/// Unknown.
+swallowing: []const u8,
+/// Unknown.
+focusHistoryID: i32,
 
 
