0f500208273d33d7ba80fc1631cf276a6080437b
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2024-08-24 20:28:24 +0200

    fix: Sockets crashing if the pipe is broken

--- src/hyprland/Socket.zig
+++ src/hyprland/Socket.zig
@@ -3,46 +3,67 @@ const util = @import("../util.zig");
 const Self = @This();
 
 path: []const u8,
-socket: std.posix.socket_t,
+socket: ?std.posix.socket_t,
 
 pub fn init( path: []const u8 ) !Self {
 	if ( path.len > 108 )
 		return error.PathTooLong;
 
-	const sock = try std.posix.socket(std.posix.AF.UNIX, std.posix.SOCK.STREAM | std.posix.SOCK.NONBLOCK, std.posix.PROT.NONE);
-	errdefer std.posix.close( sock );
-
-	var addr = std.posix.sockaddr.un{ .path = undefined };
-	@memcpy( addr.path[0..path.len], path );
-	try std.posix.connect( sock, @ptrCast(&addr), @sizeOf(@TypeOf(addr)) );
-
 	return .{
 		.path = path,
-		.socket = sock,
+		.socket = null
 	};
 }
 
 pub fn deinit( self: *Self ) void {
-	std.posix.close( self.socket );
+	self.close();
+}
+
+pub fn connect( self: *Self ) !void {
+	const sock = try std.posix.socket(std.posix.AF.UNIX, std.posix.SOCK.STREAM | std.posix.SOCK.NONBLOCK, std.posix.PROT.NONE);
+	errdefer std.posix.close( sock );
+
+	var addr = std.posix.sockaddr.un{ .path = undefined };
+	@memcpy( addr.path[0..self.path.len], self.path );
+	try std.posix.connect( sock, @ptrCast(&addr), @sizeOf(@TypeOf(addr)) );
+	self.socket = sock;
+}
+
+pub fn close( self: *Self ) void {
+	if ( self.socket ) |sock| {
+		std.posix.close( sock );
+		self.socket = null;
+	}
 }
 
 pub fn send( self: *Self, buf: []const u8 ) !void {
+	if ( self.socket == null ) {
+		return error.NotConnected;
+	}
 	// this may fail with `error.WouldBlock`, but our messages should be fairly small so we should be good
-	_ = try std.posix.send( self.socket, buf, 0 );
+	_ = try std.posix.send( self.socket orelse unreachable, buf, 0 );
 }
 
-pub fn wait( self: *Self, timeout: i32 ) !void {
+pub fn wait( self: *Self, timeout: f32 ) !void {
+	if ( self.socket == null ) {
+		return error.NotConnected;
+	}
+
 	var fds: [2]std.os.linux.pollfd = undefined;
-	fds[0].fd = self.socket;
-	fds[0].events = 0;
+	fds[0].fd = self.socket orelse unreachable;
+	fds[0].events = std.os.linux.POLL.RDNORM;
 	fds[0].revents = 0;
-	_ = try std.posix.poll( fds[0..1], timeout );
+	_ = try std.posix.poll( fds[0..1], @intFromFloat( timeout * 1000 ) );
 }
 
 pub fn read( self: *Self, buffer: []u8 ) ![]const u8 {
+	if ( self.socket == null ) {
+		return error.NotConnected;
+	}
+
 	var offset: usize = 0;
 	while ( true ) {
-		const chunk = std.posix.recv( self.socket, buffer[offset..], 0 ) catch |err| {
+		const chunk = std.posix.recv( self.socket orelse unreachable, buffer[offset..], 0 ) catch |err| {
 			if ( err == error.WouldBlock ) {
 				break;
 			}
@@ -60,35 +81,58 @@ pub fn read( self: *Self, buffer: []u8 ) ![]const u8 {
 }
 
 pub fn readAlloc( self: *Self, allocator: std.mem.Allocator ) ![]const u8 {
-	var resBuf = std.ArrayList(u8).init( allocator );
-	errdefer resBuf.deinit();
+	if ( self.socket == null ) {
+		return error.NotConnected;
+	}
+
+	var result = std.ArrayList(u8).init( allocator );
+	errdefer result.deinit();
+	try result.ensureTotalCapacity( 256 );
 
 	var buffer: [1024]u8 = undefined;
-
 	while ( true ) {
-		const chunk = std.posix.recv( self.socket, &buffer, 0 ) catch |err| {
+		const chunk = std.posix.recv( self.socket orelse unreachable, &buffer, 0 ) catch |err| {
 			if ( err == error.WouldBlock ) {
 				break;
 			}
 			// its an actual error
 			return err;
 		};
-		try resBuf.appendSlice( buffer[0..chunk] );
+
+		if ( chunk == 0 ) {
+			break;
+		}
+
+		try result.appendSlice( buffer[0..chunk] );
 	}
 
-	return resBuf.items;
+	result.shrinkAndFree( result.items.len );
+	return result.items;
 }
 
-pub fn execute( self: *Self, comptime command: []const u8, flags: []const u8, allocator: std.mem.Allocator ) ![]const u8 {
+pub fn execute( self: *Self, comptime command: []const u8, args: []const u8, allocator: std.mem.Allocator ) ![]const u8 {
 	comptime {
+		// TODO: Make this error-check args too
 		const valid: []const []const u8 = &.{ "version", "monitors", "workspaces", "activeworkspace", "workspacerules", "clients", "devices", "decorations", "binds", "activewindow", "layers", "splash", "getoption", "cursorpos", "animations", "instances", "layouts", "configerrors", "rollinglog", "locked", "descriptions" };
 		if (! util.inSlice( []const u8, valid, command ) ) {
 			@compileError( "Invalid command found in socket executor" );
 		}
 	}
-	var buffer: [256]u8 = undefined;
-	const res = try std.fmt.bufPrintZ( &buffer, "{s} {s}", .{ command, flags });
-	try self.send( res );
-	try self.wait( 1 );
+
+	// build command
+	var backBuffer: [256]u8 = undefined;
+	var cmdBuffer = std.ArrayListUnmanaged(u8).initBuffer( &backBuffer );
+	var writer = cmdBuffer.fixedWriter();
+	try writer.print( "j/{s}", .{ command } );
+	if ( args.len > 1 ) {
+		try writer.print( " {s}", .{ args } );
+	}
+
+	// send request & read output
+	std.log.debug( "Executing request `{s}`", .{ cmdBuffer.items } );
+	try self.connect();
+	defer self.close();
+	try self.send( cmdBuffer.items );
+	try self.wait( 0.5 );
 	return try self.readAlloc( allocator );
 }
